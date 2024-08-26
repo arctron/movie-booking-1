915e49ddeae9ef6aba2d1902513e3dfdeb95eabb
Abhishek Raj Chauhan <arc805@gmail.com>
2024-08-26 14:45:28 +0530

    Init Spring Boot project

---
+++ src/main/java/com/github/arctron/bookmovie/BookMovieApplication.java
@@ -0,0 +1,13 @@
+package com.github.arctron.bookmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMovieApplication.class, args);
+	}
+
+}
